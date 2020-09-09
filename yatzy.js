645112b3b126d58389fb642307a3c28d53f99e0b
Axel Thiel <axth2220@gmail.com>
2020-09-09 14:18:08 +0200

    hmm

--- yatzy.js
+++ yatzy.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let calc_button = document.getElementById("calc");
     let calc_tds = document.querySelectorAll('input[class="calc"]')
 
-    let total = 0;
-
-    for(let i = 0; i < calc_tds.length; i++){
-        total += (calc_tds[i].value) * (i + 1);
-    }
-
-    console.log(total)
+    //det här funkar bara med en spelare
 
     calc_button.addEventListener("click", function(event) {
-        console.log(name_element.value);
+        
+        let total = 0;
+
+        for (let i = 0; i < calc_tds.length; i++){
+            total += (calc_tds[i].value) * (i + 1);
+        }
+        console.log(total)
     });
-
-
 });
-
