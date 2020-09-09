b5b48abd9b608422c2c96c9b1307e5de3babdc4c
Axel Thiel <axth2220@gmail.com>
2020-09-09 13:20:52 +0200

    not a very good solution

--- yatzy.js
+++ yatzy.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     let name_element = document.getElementById("name");
     let calc_button = document.getElementById("calc");
+    let calc_tds = document.querySelectorAll('input[class="calc"]')
+
+    let total = 0;
+
+    for(let i = 0; i < calc_tds.length; i++){
+        total += (calc_tds[i].value) * (i + 1);
+    }
+
+    console.log(total)
+
     calc_button.addEventListener("click", function(event) {
         console.log(name_element.value);
     });
+
+
 });
+
