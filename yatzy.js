d8e6bb07e4d9b08788a188decb6f352598d72d48
Antonio A R <antonio.axelsson@gmail.com>
2020-09-09 14:11:38 +0200

    lade till alternativ av ifyllnad

--- yatzy.js
+++ yatzy.js
@@ -1,20 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
     let name_element = document.getElementById("name");
     let calc_button = document.getElementById("calc");
-    let calc_tds = document.querySelectorAll('input[class="calc"]')
 
-    let total = 0;
+    calc_button.addEventListener("click", function (event) {
+        let calc_tds = document.querySelectorAll('input[class="calc"]')
+        let total = 0;
+        for (let i = 0; i < calc_tds.length; i++) {
+            
+            /* funkar för input av summa av tärningar 
+            (dvs. får jag 3 st femmor fyller jag i 15) */
+            //total += Number(calc_tds[i].value);
 
-    for(let i = 0; i < calc_tds.length; i++){
-        total += (calc_tds[i].value) * (i + 1);
-    }
-
-    console.log(total)
-
-    calc_button.addEventListener("click", function(event) {
-        console.log(name_element.value);
+            /* funkar för input av antal tärningar
+            (dvs. får jag 3 st femmor fyller jag i 3) */
+            total += Number(calc_tds[i].value) * (i + 1); 
+        }
+        console.log(total);
     });
 
 
 });
-
