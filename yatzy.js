73fe6de457217fc154ae9b0048ed094870249b66
Sara Viktorsson <saraviktorsson@hotmail.com>
2020-09-09 14:20:20 +0200

    changed stuff

--- yatzy.js
+++ yatzy.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let calc_button = document.getElementById("calc");
     let calc_tds = document.querySelectorAll('input[class="calc"]')
 
-    let total = 0;
-
-    for(let i = 0; i < calc_tds.length; i++){
-        total += (calc_tds[i].value) * (i + 1);
-    }
-
-    console.log(total)
-
     calc_button.addEventListener("click", function(event) {
-        console.log(name_element.value);
+        let total = 0;
+        for(let i = 0; i < calc_tds.length; i++){
+            total += (calc_tds[i].value) * (i + 1);
+        }
+        console.log(total) // skriv ut detta i rutan bredvid summa.
     });
+
+    //behöver en class för varje spelares columner. 
+    //skape en funktion som tar in arrayen och räknar ut summan och skriver ut summan bredvid "sum",
+    
 
 
 });
