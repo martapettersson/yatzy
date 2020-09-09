6ab04084619b97e999777a09be0a29b56958ba23
Axel Thiel <axth2220@gmail.com>
2020-09-09 17:03:36 +0200

    first commit on test branch

--- yatzy.js
+++ yatzy.js
@@ -1,45 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     let name_element = document.getElementById("name");
     let calc_button = document.getElementById("calc");
-<<<<<<< HEAD
-    let calc_tds = document.querySelectorAll('input[class="calc"]');
-    
-=======
     let sum_element = document.getElementById('player1_sum');
     let bonus_element = document.getElementById('player1_bonus');
->>>>>>> a63259a75b6e10a26187dcd1ea8499b29e88f6e6
+    
 
     //det här funkar bara med en spelare
 
+    let current_class = ['calc', 'calc2', 'calc3', 'calc4'];
+
     calc_button.addEventListener("click", function(event) {
-        
-        let total = 0;
-<<<<<<< HEAD
 
-        for (let i = 0; i < calc_tds.length; i++){
-            total += (calc_tds[i].value) * (i + 1);
-        }
-        console.log(total)
-    });
-});
-=======
-        for (let i = 0; i < calc_tds.length; i++) {
+        for (let i = 0; i < current_class.length; i++) {
+
+            let calcs_td = document.querySelectorAll(`input[class="${current_class[i]}"]`)
             
-            /* funkar för input av summa av tärningar 
-            (dvs. får jag 3 st femmor fyller jag i 15) */
-            total += Number(calc_tds[i].value);
+            let total = 0;
 
-            /* funkar för input av antal tärningar
-            (dvs. får jag 3 st femmor fyller jag i 3) */
-            //total += Number(calc_tds[i].value) * (i + 1); 
-        }
-        sum_element.value = total;
+            for (let i = 0; i < calc_tds.length; i++) {
+                total += Number(calc_tds[i].value)
+            }
 
-        if(total >= 63){
-            bonus_element.value = 50;
-        }else{
-            bonus_element.value = 0;
-        }
+            sum_element.value = total; //här blir det problem
+
+            if(total >= 63){
+                bonus_element.value = 50;
+            }else{
+                bonus_element.value = 0;
+            }
+        };
 
     });
 
@@ -49,4 +38,3 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 
 });
->>>>>>> a63259a75b6e10a26187dcd1ea8499b29e88f6e6
