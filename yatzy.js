bfeab6d49d3e6efe90d595c076588398f83d1d47
Marta Pettersson <marta.pettersson@gmail.com>
2020-09-11 15:23:04 +0200

    lite struktur

--- yatzy.js
+++ yatzy.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    let name_element = document.getElementById("name");
     let calc_button = document.getElementById("calc");
     let sum_element = document.getElementById('player1_sum');
     let bonus_element = document.getElementById('player1_bonus');
@@ -8,28 +7,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
         let calc_tds = document.querySelectorAll('input[class="calc"]')
         let total = 0;
         for (let i = 0; i < calc_tds.length; i++) {
-            
-            /* funkar för input av summa av tärningar 
-            (dvs. får jag 3 st femmor fyller jag i 15) */
+
             total += Number(calc_tds[i].value);
 
-            /* funkar för input av antal tärningar
-            (dvs. får jag 3 st femmor fyller jag i 3) */
-            //total += Number(calc_tds[i].value) * (i + 1); 
         }
         sum_element.value = total;
 
         if(total >= 63){
             bonus_element.value = 50;
-        }else{
+        }
+        else{
             bonus_element.value = 0;
         }
 
     });
-
-    //behöver en class för varje spelares columner. 
-    //skape en funktion som tar in arrayen och räknar ut summan och skriver ut summan bredvid "sum",
-    
-
 
 });
