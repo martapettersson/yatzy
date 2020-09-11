5158ab82fde91aa17c352b835a261761c888f03a
Axel Thiel <axth2220@gmail.com>
2020-09-11 15:28:38 +0200

    fixa det här

---
+++ funktion.js
@@ -0,0 +1,9 @@
+let calc_tds = document.querySelectorAll(`input[class="player1"]`)
+let player1_sum = document.getElementById('player1_sum')
+
+
+calc_tds.forEach(function(element){
+    element.addEventListener('change', function(e){
+        player1_sum.value += this.value;
+    })
+})
--- yatzy.js
+++ yatzy.js
@@ -1,33 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-    let name_element = document.getElementById("name");
-    let calc_button = document.getElementById("calc");
-    let bonus_element = document.getElementById('player1_bonus');
+//     let name_element = document.getElementById("name");
+//     let calc_button = document.getElementById("calc");
+//     let bonus_element = document.getElementById('player1_bonus');
 
-    let players = ['player1', 'player2', 'player3', 'player4']; 
+//     let players = ['player1', 'player2', 'player3', 'player4']; 
 
-    calc_button.addEventListener("click", function(event) {
+
+//     let calc_tds = document.querySelectorAll(`input[class="player1"]`)
+
+
+let calc_tds = document.querySelectorAll(`input[class="player1"]`)
+let player1_sum = document.getElementById('player1_sum')
+let sum = 0;
+
+calc_tds.forEach(function(element){
+    element.addEventListener('change', function(e){ 
+        sum += Number(this.value)      
+        player1_sum.value = sum;
+    })
+})
+
+//variabel sum = 0
+
+
+
+//     calc_button.addEventListener("click", function(event) {
 
         
-        for (let i = 0; i < players.length; i++) {
+//         for (let i = 0; i < players.length; i++) {
 
-            let total = 0;
-            let bonus_element = document.getElementById(`${players[i]}_bonus`);
-            let sum_element = document.getElementById(`${players[i]}_sum`);
-            let calc_tds = document.querySelectorAll(`input[class="${players[i]}"]`)
+//             let total = 0;
+//             let bonus_element = document.getElementById(`${players[i]}_bonus`);
+//             let sum_element = document.getElementById(`${players[i]}_sum`);
+//             let calc_tds = document.querySelectorAll(`input[class="${players[i]}"]`)
 
-            for (let i = 0; i < calc_tds.length; i++) {
-                total += Number(calc_tds[i].value)
-            }
+//             for (let i = 0; i < calc_tds.length; i++) {
+//                 total += Number(calc_tds[i].value)
+//             }
 
-            sum_element.value = total;
+//             sum_element.value = total;
 
-            if (total >= 63){
-                bonus_element.value = 50;
-            } else {
-                bonus_element.value = 0;
-            }
+//             if (total >= 63){
+//                 bonus_element.value = 50;
+//             } else {
+//                 bonus_element.value = 0;
+//             }
         
-        };
-    });    
+//         };
+//     });    
 
 });
