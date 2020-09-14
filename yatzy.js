a51f410402964fe92a81b5a290c993bd8696bea0
Marta Pettersson <marta.pettersson@gmail.com>
2020-09-14 19:43:57 +0200

    playing around

--- yatzy.js
+++ yatzy.js
@@ -1,15 +1,37 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    let player1_ints = document.querySelectorAll('input[class="player1"]')
+    let player1_ints = document.querySelectorAll(".player1_ints");
     let player1_sum = document.getElementById("player1_sum");
-
-    player1_ints.forEach(function(element){
-        element.addEventListener('change', function(e){ 
+    
+    player1_ints.forEach(function (player1_int) {
+        player1_int.addEventListener('change', function (e) {
             sum = 0;
-            for(let i = 0; player1_ints.length > i; i++){
-                sum+= (Number(player1_ints[i].value));
+            for (let i = 0; i < player1_ints.length; i++) {
+                sum += Number(player1_ints[i].value);
             }
             player1_sum.value = sum;
-        })
-    })
+            console.log(sum);
+        });
+    });
+    
+    let 
 
 });
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function (e) {
+//     let player1_ints = document.querySelectorAll(`input[class="player1_ints"]`);
+//     let player1_sum = document.getElementById("player1_sum");
+
+//     player1_ints.forEach(function (player1_int) {
+//         player1_int.addEventListener('change', function (e) {
+//             sum = 0;
+//             for (let i = 0; i < player1_ints.length; i++) {
+//                 sum += (Number(player1_ints[i].value));
+//             }
+//             player1_sum.value = sum;
+//         });
+//     });
+
+// });
