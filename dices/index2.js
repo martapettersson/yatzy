3b0c0bac17a6800b92c5fc8808c48725efb7cb4d
Axel Thiel <axth2220@gmail.com>
2020-09-16 14:35:56 +0200

    added dices and dice function

---
+++ dices/index2.js
@@ -0,0 +1,18 @@
+let yatzyControl = {
+    dices: {
+        dice1: {
+            id: 1,
+            diceValue: 0
+        },
+        dice
+    },
+
+    checkboxes: {
+        checkbox1: {
+            checked: false
+        }
+    }
+}
+
+//loop som körs 5 ggr.
+//lägg till alla dices och
