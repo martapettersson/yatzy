9a5cbb93339c483ba70b52cff26e081d024d4a45
Axel Thiel <axth2220@gmail.com>
2020-09-22 18:18:09 +0200

    lade till klasser

--- funktion.js
+++
@@ -1,9 +0,0 @@
-let calc_tds = document.querySelectorAll(`input[class="player1"]`)
-let player1_sum = document.getElementById('player1_sum')
-
-
-calc_tds.forEach(function(element){
-    element.addEventListener('change', function(e){
-        player1_sum.value += this.value;
-    })
-})
