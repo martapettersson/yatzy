616284663d74acdb3676eec5cf8ce900927bc4cd
Sara Viktorsson <saraviktorsson@hotmail.com>
2020-09-23 12:48:22 +0200

    moved everything inside classes

--- yatzy.js
+++ yatzy.js
@@ -1,79 +1,88 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
-    let throwDiceButton = document.getElementById('throwDice');
-    let oneToSixCollection = document.querySelectorAll('input.player1');
-    //console.log(oneToSixCollection);
+//----------------class för en tärning----------------
+class Die {
+    constructor(){
+        this.value = this.new_value();
+    }
 
-    //lägg till eventlistener på alla tds, så fort yatzytabellen ändras uppdateras också summafältet + bonusfältet:
-    oneToSixCollection.forEach(function(element){
-        element.addEventListener('change', function(e){
+    new_value(){
+        return Math.floor(Math.random() * 6) +1;
+    }
+}
+ //--------------class för tärningsuppsättning---------
+class Dice {
+    constructor(){
+        this.throw = this.throwDice();
+    }
 
-            //skapa en ny array med map från oneToSixCollection (html collection)
-            let oneToSixArr = Array.from(oneToSixCollection);
-            let oneToSixArray = oneToSixArr.map((num) => {
-            return Number(num.value);
-            });
-            console.log(oneToSixArray);
+    //metod som skriver ut tärningsvärden för de tärningar som ej är icheckade
+    //uppdaterar även hur många slag spelar har kvar i denna omgång
+    throwDice(){
+        this.diceValue = document.querySelectorAll('.dice_value');
+        this.checkbox = document.querySelectorAll('.checkbox');
 
-            // använd reduce för att få summarn från arrayen oneToSixArray och skriva ut den i sum:
-            let reducedArray = oneToSixArray.reduce((sum, currValue) => {
-                return sum + currValue;
-            }, 0);
-            console.log(reducedArray);
-
-            //skriver ut summan i "sum"-fältet:
-            let player1_sum = document.getElementById('player1_sum');
-            player1_sum.value = reducedArray;
-
-            //bonusfältet fylls i med 50 ifall summafältet är mer är 63:
-            let player1_bonus = document.getElementById('player1_bonus');
-            if(player1_sum.value >= 63){
-                player1_bonus.value = 50;
-            } else{
-                player1_bonus.value = 0;
-            }
-         });
-     });
-
-    //add eventlistener på knappen. när knappen trycks ges random nummer till tillhörande labels
-    let timesThrown = 0; //håller koll på antal slag
-    throwDiceButton.addEventListener('click', function(element){
-        let diceValue = document.querySelectorAll('.dice_value');
-        let checkbox = document.querySelectorAll('.checkbox');
-        for(i = 0; i <5; i++){ //loopar igenom alla checkboxar/tärningsvärden, om inte boxen på nuvarande index är kyssad: ge värdet på samma index ett random nummer
-            if(!checkbox[i].checked){
-                let random = Math.floor((Math.random() * 6)+1);
-                diceValue[i].innerHTML = random;
+        for(let i = 0; i <5; i++){ //loopar igenom alla checkboxar/tärningsvärden, om inte boxen på nuvarande index är kyssad: ge värdet på samma index ett random nummer
+            if(!this.checkbox[i].checked){
+                let random = new Die().value; 
+                this.diceValue[i].innerHTML = random;
             }
         }
+        
         timesThrown++;
         if(timesThrown >= 3){ // när man slagit tre slag fungerar inte knappen längre
             throwDiceButton.disabled = true;
         } 
+    }
+    //lägg till andra tärningsmetoder här, fullhouse osv.. 
+    
+}
+
+//--------------------class gameBoard -----------------------------
+
+class Gameboard {
+    constructor(){
+        this.oneToSixCollection = document.querySelectorAll('input.player1');
+        this.oneToSixCollToArr = Array.from(this.oneToSixCollection);
+        this.oneToSixArray = this.oneToSixCollToArr.map((num) => { // behöver ej göra om med map?
+        return Number(num.value);
+        });
+
+        this.sum = this.calculateSum();
+        this.bonus = this.checkBonus();
+    }
+
+    calculateSum(){
+        this.reducedArray = this.oneToSixArray.reduce((sum, currValue) => {
+            return sum + currValue;
+        }, 0);
+
+        this.player1_sum = document.getElementById('player1_sum');
+        this.player1_sum.value = this.reducedArray;
+    }
+
+    checkBonus(){
+        this.player1_bonus = document.getElementById('player1_bonus');
+        if(this.player1_sum.value >= 63){
+            this.player1_bonus.value = 50;
+        } else{
+            this.player1_bonus.value = 0;
+        }
+    }
+}
+
+   //knappen startar allt
+    let throwDiceButton = document.getElementById('throwDice');
+
+    //håller koll på antal slag en spelare har per runda
+    let timesThrown = 0; 
+
+    // när knappen trycks uppdateras gameBoard och tärningsformuläret. 
+    throwDiceButton.addEventListener('click', function(element){ 
+    
+    let newDice = new Dice();
+    let game = new Gameboard();
+       
     });
-
-
-
     
-    
-    
-    
-    
-
-
-
-    
-    
-    
-
-
-
-
-    
-
-    
-    
-    
-
-
 });
